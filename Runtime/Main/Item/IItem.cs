93a763539d7b7be5f3dae3f4f0e24a36086fcdb7
rob1997 <robelgetnetrg@gmail.com>
2022-10-02 10:28:10 +0300

    GUID Support

--- Runtime/Main/Item/IItem.cs
+++ Runtime/Main/Item/IItem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ namespace Inventory.Main.Item
 {
     public interface IItem
     {
+        Guid Id { get; set; }
+        
         ItemReference Reference { get; }
 
         T Clone<T>() where T : IItem;
--- Runtime/Main/Item/Item.cs
+++ Runtime/Main/Item/Item.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using Core.Utils;
 using UnityEngine;
 
 namespace Inventory.Main.Item
@@ -11,12 +12,35 @@ namespace Inventory.Main.Item
     {
         [SerializeField] protected T reference;
 
+        [SerializeField] [HideInInspector] private string id = Utils.NewGuid();
+
+        public Guid Id
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(id))
+                {
+                    Debug.LogError("Item Guid NullOrEmpty, Initializing...");
+                    
+                    id = Utils.NewGuid();
+                }
+
+                return new Guid(id);
+            }
+
+            set => id = value.ToString();
+        }
+        
         public ItemReference Reference => reference;
 
         public TItem Clone<TItem>() where TItem : IItem
         {
             //soft clone/copy...maintain references
-            return (TItem) this.MemberwiseClone();
+            TItem item = (TItem) MemberwiseClone();
+            
+            item.Id = new Guid(Utils.NewGuid());
+            
+            return item;
         }
     }
 }
